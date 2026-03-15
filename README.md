6c4138434ced4d6a5ee04f02c8452c611a15c92e
hqzing <hqzing@outlook.com>
2026-03-15 19:46:15 +0800

    第一个版本

--- README.md
+++ README.md
@@ -1,1 +1,105 @@
 # ohos-vim
+
+本项目为 OpenHarmony 平台编译了 vim，并发布预构建包。
+
+## 获取预构建包
+
+前往 [release 页面](https://github.com/Harmonybrew/ohos-vim/releases) 获取。
+
+## 用法
+**1\. 在鸿蒙 PC 中使用**
+
+因系统安全规格限制等原因，暂不支持通过“解压 + 配 PATH” 的方式使用这个软件包。
+
+你可以尝试将 tar 包打成 hnp 包再使用，详情请参考 [DevBox](https://gitcode.com/OpenHarmonyPCDeveloper/devbox) 的方案。
+
+**2\. 在鸿蒙开发板中使用**
+
+用 hdc 把它推到设备上，然后以“解压 + 配 PATH” 的方式使用。
+
+示例：
+```sh
+hdc file send vim-9.2.0150-ohos-arm64.tar.gz /data
+hdc shell
+
+cd /data
+tar -zxf vim-9.2.0150-ohos-arm64.tar.gz
+export PATH=/data/vim-9.2.0150-ohos-arm64/bin:$PATH
+export TERM=screen-256color
+
+# 现在可以使用 vim 命令了
+```
+
+在 hdc shell 环境中使用 vim，需要多一个 `export TERM=screen-256color` 的步骤。因为 hdc 在连接 OpenHarmony 设备时会创建一个伪终端（pseudo-terminal），它的行为并不完全像一个标准的 xterm 或 xterm-256color 终端，而更接近于 screen 或 tmux 这类多路复用器的终端模拟方式。我们必须手动设置一个有效的 TERM 类型，hdc 发送的键码和 terminfo 定义的键码才能匹配，才不会出现按键错位。
+
+**3\. 在 [鸿蒙容器](https://github.com/hqzing/docker-mini-openharmony) 中使用**
+
+在容器中用 curl 下载这个软件包，然后以“解压 + 配 PATH” 的方式使用。
+
+示例：
+```sh
+docker run -itd --name=ohos ghcr.io/hqzing/docker-mini-openharmony:latest
+docker exec -it ohos sh
+
+cd /root
+curl -L -O https://github.com/Harmonybrew/ohos-vim/releases/download/9.2.0150/vim-9.2.0150-ohos-arm64.tar.gz
+tar -zxf vim-9.2.0150-ohos-arm64.tar.gz -C /opt
+export PATH=/opt/vim-9.2.0150-ohos-arm64/bin:$PATH
+
+# 现在可以使用 vim 命令了
+```
+
+## 从源码构建
+
+**1\. 手动构建**
+
+这个项目使用本地编译（native compilation，也可以叫本机编译或原生编译）的做法来编译鸿蒙版 vim，而不是交叉编译。
+
+需要在 [鸿蒙容器](https://github.com/hqzing/docker-mini-openharmony) 中运行项目里的 build.sh，以实现 vim 的本地编译。
+
+示例：
+```sh
+git clone https://github.com/Harmonybrew/ohos-vim.git
+cd ohos-vim
+
+docker run \
+  --rm \
+  -it \
+  -v "$PWD":/workdir \
+  -w /workdir \
+  ghcr.io/hqzing/docker-mini-openharmony:latest \
+  ./build.sh
+```
+
+**2\. 使用流水线构建**
+
+如果你熟悉 GitHub Actions，你可以直接复用项目内的工作流配置，使用 GitHub 的流水线来完成构建。
+
+这种情况下，你使用的是 GitHub 提供的构建机，不需要自己准备构建环境。
+
+只需要这么做，你就可以进行你的个人构建：
+1. Fork 本项目，生成个人仓
+2. 在个人仓的“Actions”菜单里面启用工作流
+3. 在个人仓提交代码或发版本，触发流水线运行
+
+## 常见问题
+
+
+**1\. 语法高亮和按键功能异常**
+
+vim 的正常工作依赖 terminfo 数据库，如果 vim 找不到 terminfo 数据库，就有可能产生语法高亮和按键功能异常。
+
+本项目在构建 vim 时将默认的 terminfo 搜索路径设置为：
+```text
+/opt/vim-9.2.0150-ohos-arm64/share/terminfo:/data/vim-9.2.0150-ohos-arm64/share/terminfo
+```
+
+如果你在使用过程中没有将 vim 解压到 /opt 或 /data 目录下，且系统中不存在有效的 `TERMINFO` 环境变量，vim 将会搜索不到任何 terminfo 数据库，将无法正常工作。
+
+这种情况下，你需要设置 `TERMINFO` 环境变量，强制指定 vim 去它的安装目录中搜索自带的 terminfo 数据库。
+
+```sh
+export TERMINFO=<vim安装目录的绝对路径>/share/terminfo
+```
+
+注意，这个变量不仅会影响 vim，也会影响其他依赖 terminfo 的程序，请谨慎使用。
---
+++ build.sh
@@ -0,0 +1,164 @@
+#!/bin/sh
+set -e
+
+WORKDIR=$(pwd)
+
+# 如果存在旧的目录和文件，就清理掉
+# 仅清理工作目录，不清理系统目录，因为默认用户每次使用新的容器进行构建（仓库中的构建指南是这么指导的）
+rm -rf *.tar.gz \
+    deps \
+    vim-9.2.0150 \
+    vim-9.2.0150-ohos-arm64
+
+# 下载一些命令行工具，并将它们软链接到 bin 目录中
+cd /opt
+echo "coreutils 9.10
+busybox 1.37.0
+grep 3.12
+gawk 5.3.2
+tar 1.35
+gzip 1.14
+make 4.4.1" >/tmp/tools.txt
+while read -r name ver; do
+    curl -fSLO https://github.com/Harmonybrew/ohos-$name/releases/download/$ver/$name-$ver-ohos-arm64.tar.gz
+done </tmp/tools.txt
+ls | grep tar.gz$ | xargs -n 1 tar -zxf
+ln -sf $(pwd)/*-ohos-arm64/bin/* /bin/
+
+# 准备鸿蒙版 ohos-sdk
+sdk_download_url="https://cidownload.openharmony.cn/version/Master_Version/ohos-sdk-public_ohos/20251209_020142/version-Master_Version-ohos-sdk-public_ohos-20251209_020142-ohos-sdk-public_ohos.tar.gz"
+curl -fSL -o ohos-sdk.tar.gz $sdk_download_url
+mkdir /opt/ohos-sdk
+tar -zxf ohos-sdk.tar.gz -C /opt/ohos-sdk
+rm -f ohos-sdk.tar.gz
+cd /opt/ohos-sdk/ohos
+busybox unzip -q native-*.zip
+rm -rf *.zip
+
+# 把 llvm 里面的命令封装一份放到 /bin 目录下，只封装必要的工具。
+# 为了照顾 clang （clang 软链接到其他目录使用会找不到 sysroot），
+# 对所有命令统一用这种封装的方案，而非软链接。
+essential_tools="clang
+clang++
+clang-cpp
+ld.lld
+lldb
+llvm-addr2line
+llvm-ar
+llvm-cxxfilt
+llvm-nm
+llvm-objcopy
+llvm-objdump
+llvm-ranlib
+llvm-readelf
+llvm-size
+llvm-strings
+llvm-strip"
+for executable in $essential_tools; do
+    cat <<EOF > /bin/$executable
+#!/bin/sh
+exec /opt/ohos-sdk/ohos/native/llvm/bin/$executable "\$@"
+EOF
+    chmod 0755 /bin/$executable
+done
+
+# 把 llvm 软链接成 cc、gcc 等命令
+cd /bin
+ln -s clang cc
+ln -s clang gcc
+ln -s clang++ c++
+ln -s clang++ g++
+ln -s ld.lld ld
+ln -s llvm-addr2line addr2line
+ln -s llvm-ar ar
+ln -s llvm-cxxfilt c++filt
+ln -s llvm-nm nm
+ln -s llvm-objcopy objcopy
+ln -s llvm-objdump objdump
+ln -s llvm-ranlib ranlib
+ln -s llvm-readelf readelf
+ln -s llvm-size size
+ln -s llvm-strip strip
+
+VIM_PREFIX="/opt/vim-9.2.0150-ohos-arm64"
+mkdir $WORKDIR/deps
+cd $WORKDIR/deps
+
+# 编译 ncurses
+curl -fSLO https://ftp.gnu.org/gnu/ncurses/ncurses-6.5.tar.gz
+tar -zxf ncurses-6.5.tar.gz
+cd ncurses-6.5
+./configure \
+    --prefix=/opt/deps \
+    --enable-termcap \
+    --enable-widec \
+    --without-shared \
+    --with-terminfo-dirs=$VIM_PREFIX/share/terminfo:/data/vim-9.2.0150-ohos-arm64/share/terminfo
+make -j$(nproc)
+make install
+cd ..
+
+# 让 libncursesw 提供兼容 libncurses 的头文件和库文件
+cd /opt/deps/include
+ln -s ncursesw ncurses 
+ln -s ncursesw/*.h .
+cd /opt/deps/lib
+ln -s libformw.a libform.a
+ln -s libmenuw.a libmenu.a
+ln -s libncurses++w.a libncurses++.a
+ln -s libncursesw.a libncurses.a
+ln -s libpanelw.a libpanel.a
+
+cd $WORKDIR
+
+# 编译 vim
+export CPPFLAGS="-I/opt/deps/include"
+export LDFLAGS="-L/opt/deps/lib"
+curl -fSL -o vim-9.2.0150.tar.gz https://github.com/vim/vim/archive/refs/tags/v9.2.0150.tar.gz
+tar -zxf vim-9.2.0150.tar.gz
+cd vim-9.2.0150
+./configure \
+    --prefix=$VIM_PREFIX \
+    --enable-multibyte \
+    --with-tlib=ncurses \
+    --enable-cscope \
+    --enable-terminal \
+    --disable-gui \
+    --without-x
+make -j$(nproc)
+make install prefix=$VIM_PREFIX
+cd ..
+
+# 默认禁用鼠标支持
+sed -i 's/set mouse=a/set mouse=/g' $VIM_PREFIX/share/vim/vim92/defaults.vim
+sed -i 's/set mouse=nvi/set mouse=/g' $VIM_PREFIX/share/vim/vim92/defaults.vim
+
+# 将 terminfo 数据库携带到制品中
+cp -r /opt/deps/share/terminfo $VIM_PREFIX/share/
+
+# 履行开源义务，把使用的开源软件的 license 全部聚合起来放到制品中
+cat <<EOF > $VIM_PREFIX/licenses.txt
+This document describes the licenses of all software distributed with the
+bundled application.
+==========================================================================
+
+vim
+=============
+$(cat vim-9.2.0150/LICENSE)
+
+ncurses
+=============
+==license==
+$(cat deps/ncurses-6.5/COPYING)
+==authors==
+$(cat deps/ncurses-6.5/AUTHORS)
+EOF
+
+# 打包最终产物
+cp -r $VIM_PREFIX ./
+tar -zcf vim-9.2.0150-ohos-arm64.tar.gz vim-9.2.0150-ohos-arm64
+
+# 这一步是针对手动构建场景做优化。
+# 在 docker run --rm -it 的用法下，有可能文件还没落盘，容器就已经退出并被删除，从而导致压缩文件损坏。
+# 使用 sync 命令强制让文件落盘，可以避免那种情况的发生。
+sync
